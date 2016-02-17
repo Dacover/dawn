4f0f779363f30e17b0f9744aaadf4fd2782431a0
Dachuan <935148040@qq.com>
2016-02-17 18:53:48 +0800

    Initial Commit

---
+++ dawn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dawn
+//
+//  Created by 京瑞集团 on 16/2/17.
+//  Copyright © 2016年 京瑞集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dawn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dawn
+//
+//  Created by 京瑞集团 on 16/2/17.
+//  Copyright © 2016年 京瑞集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
